6945545a3cfdd363622cf9e25a1e2dc0fccf67d2
jeckel <jeckel@jeckel.fr>
2015-11-12 22:17:49 +0100

    Save and reload configured image size

---
+++ admin.php
@@ -0,0 +1,14 @@
+<div class="wrap">
+    <?php echo "<h2>" . __('WP Recent Post options', 'wprpo_trdom' ) . "</h2>"; ?>
+     
+    <form name="wprpo_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+        <input type="hidden" name="wprpo_hidden" value="Y">
+        <?php    echo "<h4>" . __( 'Featured image size', 'wprpo_trdom' ) . "</h4>"; ?>
+        <p><?php _e("Image width : "); ?><input type="text" name="wprpo_width" value="<?php echo $this->config['width']; ?>" size="20"><?php _e(" ex: 300" ); ?></p>
+        <p><?php _e("Image height : "); ?><input type="text" name="wprpo_height" value="<?php echo $this->config['height']; ?>" size="20"><?php _e(" ex: 150" ); ?></p>
+        <hr />
+        <p class="submit">
+        <input type="submit" name="Submit" value="<?php _e('Update Options', 'wprpo_trdom' ) ?>" />
+        </p>
+    </form>
+</div>
